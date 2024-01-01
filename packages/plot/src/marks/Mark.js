374f70d38af540f0a15f3a1980cd541e9aa1bb69
Francisco Rivera <frtennis1@gmail.com>
2024-01-01 14:55:36 -0500

    feat: allow window functions in groupby by adding with statement

--- packages/plot/src/marks/Mark.js
+++ packages/plot/src/marks/Mark.js
@@ -139,7 +139,7 @@ export class Mark extends MosaicClient {
   query(filter = []) {
     if (this.hasOwnData()) return null;
     const { channels, source: { table } } = this;
-    return markQuery(channels, table).where(filter);
+    return markQuery(channels, table, filter);
   }
 
   queryPending() {
@@ -177,8 +177,9 @@ export function channelOption(c) {
     : c.as;
 }
 
-export function markQuery(channels, table, skip = []) {
-  const q = Query.from({ source: table });
+export function markQuery(channels, table, filter, skip = []) {
+  const q_with = Query.from({ source: table }).select('*');
+  const q = Query.from('__mosaicTemp');
   const dims = new Set;
   let aggr = false;
 
@@ -191,14 +192,16 @@ export function markQuery(channels, table, skip = []) {
     } else if (field) {
       if (field.aggregate) {
         aggr = true;
+        q.select({ [as]: field });
       } else {
         if (dims.has(as)) continue;
         dims.add(as);
+	q_with.select({ [as]: field });
+        q.select({ [as]: as });
       }
-      q.select({ [as]: field });
     }
   }
-
+  q.with({__mosaicTemp: q_with.where(filter)});
   if (aggr) {
     q.groupby(Array.from(dims));
   }
