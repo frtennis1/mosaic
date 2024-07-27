4e4af8adb8b02dd7a8284c41d538ba6a7ecede57
Francisco Rivera <frtennis1@gmail.com>
2024-07-27 17:29:59 -0400

    hack: do not minify index.js in widget for easier debugging

---
+++ packages/widget/vite.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import anywidget from "@anywidget/vite";
+
+export default defineConfig({
+  build: {
+    outDir: "mosaic_widget/static",
+    lib: {
+      entry: ["src/index.js"],
+      formats: ["es"],
+    },
+    minify: false,
+  },
+  plugin: [anywidget()],
+});
